0ce0003a804ef89a20cd684403f84daea1428a5d
Yaminim07 <yaminim@fusioncharts.com>
2019-09-27 12:45:58 +0530

    added keys

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  google: {
+    clientID: '992961314784-8tch16pc59qlu0psr0cgbcq231fqk8se.apps.googleusercontent.com',
+    clientSecret: '6UGB2jER1EnAiR8mNrbkHWBs'
+  },
+  
+  mongodb: {
+    dbURI: 'mongodb+srv://yaminim:dL1KxIyamC85IWpe@cluster0-ofkrb.mongodb.net/test?retryWrites=true&w=majority'
+  }
+};
